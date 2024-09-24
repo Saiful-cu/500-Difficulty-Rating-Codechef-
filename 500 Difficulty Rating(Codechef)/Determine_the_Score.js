b8eebf56c27c5496d9702da7c3adeec5ed136c56
Md. Saiful Islam <saiful.cu.19@gmail.com>
2024-09-24 07:32:34 +0600

    Determine the Score

---
+++ 500 Difficulty Rating(Codechef)/Determine_the_Score.js
@@ -0,0 +1,10 @@
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', (input)=>{
+    const nums = input.trim().split('\n');
+    const T = parseInt(nums[0])
+    for(let i=1;i<=T;i++){
+        const[X,Y] = nums[i].split(' ').map(Number);
+        let singlePoint = X/10;
+        console.log(singlePoint*Y)
+    }
+})
